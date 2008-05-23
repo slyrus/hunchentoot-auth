19af5022af12ded3cb400444aec116b1d8595f61
Cyrus Harmon <ch-lisp@bobobeach.com>
2008-05-23 08:32:28 -0700

    removing hunchentoot-macro, adding some in-progress auth stuff that is commented out.

--- hunchentoot-auth.lisp
+++ hunchentoot-auth.lisp
@@ -98,63 +98,6 @@ directing the output to *standard-output* and setting :prologue to t."
                   (make-hash-table))))
     (setf (gethash realm hash) value)))
 
-(defmacro with-unique-names ((&rest bindings) &body body)
-  "Syntax: WITH-UNIQUE-NAMES ( { var | (var x) }* ) declaration* form*
-
-Executes a series of forms with each VAR bound to a fresh,
-uninterned symbol. The uninterned symbol is as if returned by a call
-to GENSYM with the string denoted by X - or, if X is not supplied, the
-string denoted by VAR - as argument.
-
-The variable bindings created are lexical unless special declarations
-are specified. The scopes of the name bindings and declarations do not
-include the Xs.
-
-The forms are evaluated in order, and the values of all but the last
-are discarded \(that is, the body is an implicit PROGN)."
-  ;; reference implementation posted to comp.lang.lisp as
-  ;; <cy3bshuf30f.fsf@ljosa.com> by Vebjorn Ljosa - see also
-  ;; <http://www.cliki.net/Common%20Lisp%20Utilities>
-  `(let ,(mapcar #'(lambda (binding)
-                     (check-type binding (or cons symbol))
-                     (if (consp binding)
-                       (destructuring-bind (var x) binding
-                         (check-type var symbol)
-                         `(,var (gensym ,(etypecase x
-                                          (symbol (symbol-name x))
-                                          (character (string x))
-                                          (string x)))))
-                       `(,binding (gensym ,(symbol-name binding)))))
-                 bindings)
-         ,@body))
-
-(defmacro with-rebinding (bindings &body body)
-  "Syntax: WITH-REBINDING ( { var | (var prefix) }* ) form*
-
-Evaluates a series of forms in the lexical environment that is
-formed by adding the binding of each VAR to a fresh, uninterned
-symbol, and the binding of that fresh, uninterned symbol to VAR's
-original value, i.e., its value in the current lexical environment.
-
-The uninterned symbol is created as if by a call to GENSYM with the
-string denoted by PREFIX - or, if PREFIX is not supplied, the string
-denoted by VAR - as argument.
-
-The forms are evaluated in order, and the values of all but the last
-are discarded \(that is, the body is an implicit PROGN)."
-  ;; reference implementation posted to comp.lang.lisp as
-  ;; <cy3wv0fya0p.fsf@ljosa.com> by Vebjorn Ljosa - see also
-  ;; <http://www.cliki.net/Common%20Lisp%20Utilities>
-  (loop for binding in bindings
-        for var = (if (consp binding) (car binding) binding)
-        for name = (gensym)
-        collect `(,name ,var) into renames
-        collect ``(,,var ,,name) into temps
-        finally (return `(let ,renames
-                          (with-unique-names ,bindings
-                            `(let (,,@temps)
-                              ,,@body))))))
-
 (defmacro authorized-page ((realm
                             user
                             password
@@ -183,3 +126,25 @@ are discarded \(that is, the body is an implicit PROGN)."
                       (parse-host-name-and-port (host))
                     `(:host ,host-name :port ,,ssl-port)))))))
 
+
+#+nil
+(defun create-handler-authorization-handler (realm handler)
+  (unless realm 
+          (error "~S must be a realm." realm))
+  (unless handler 
+          (error "~S must be a handler." handler))
+  (flet ((handler ()
+           (let* ((script-name (url-decode (script-name)))
+                  (script-path (enough-url (regex-replace-all "\\\\" script-name "/")
+                                           uri-prefix))
+                  (script-path-directory (pathname-directory script-path)))
+             (unless (or (stringp script-path-directory)
+                         (null script-path-directory)
+                         (and (listp script-path-directory)
+                              (eq (first script-path-directory) :relative)
+                              (loop for component in (rest script-path-directory)
+                                    always (stringp component))))
+               (setf (return-code) +http-forbidden+)
+               (throw 'handler-done nil))
+             (handle-static-file (merge-pathnames script-path base-path) content-type))))
+    (create-prefix-dispatcher uri-prefix #'handler)))
